f2d384b28acd39d2e43d1d09d2ade95ad96007fd
nmtuanlx05 <manhtuanlx2005@gmail.com>
2025-08-19 02:44:40 +0700

    updateUI(Create Section inFo )

---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,26 @@
+import { dataInfo } from '@components/Info/constant';
+import InfoCard from '@components/Info/InfoCard/InfoCard';
+import MainLayout from '@components/MainLayout/MainLayout';
+import styles from './styles.module.scss';
+
+function Info() {
+    const { container } = styles;
+    return (
+        <div>
+            <MainLayout>
+                <div className={container}>
+                    {dataInfo.map((item) => (
+                        <InfoCard
+                            content={item.title}
+                            description={item.description}
+                            src={item.src}
+                        />
+                    ))}
+                </div>
+            </MainLayout>
+            ;
+        </div>
+    );
+}
+
+export default Info;
