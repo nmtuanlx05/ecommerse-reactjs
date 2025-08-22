347e9546353ca7899a433ab8d2becf3ae72ce13f
nmtuanlx05 <manhtuanlx2005@gmail.com>
2025-08-22 17:25:48 +0700

    Confixed Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import HomePage from '@components/HomePage/HomePage';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routers from '@/Routers/router';
+import { Suspense } from 'react';
 function App() {
     return (
-        <>
-            <HomePage />
-        </>
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {routers.map((item, index) => (
+                        <Route
+                            key={index}
+                            path={item.path}
+                            element={<item.component />}
+                        />
+                    ))}
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
     );
 }
 
